815976e6a4493cf80077120fcb2a6432f8e7f67e
Gerald Halomoan Samosir <geralddeveloper95@gmail.com>
2017-11-11 17:21:16 +0700

    test

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var express = require('express')
 var app = express()
 
 app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
 
 const knex =  require('knex')({
     client: 'mysql',
@@ -50,7 +49,7 @@ app.get('/', function(request, response) {
   response.send('Hello World!')
 })
 
-app.get("/test",ControllerUser.getall)
+app.get("/listUser",ControllerUser.getall)
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
